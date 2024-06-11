f9a9f7eb16f1154d76580f69e658c9967ab7aecc
juanerv12 <juanervaldi@gmail.com>
2024-06-11 11:35:18 -0500

    Update Habitaciones.java
    se incluyen comentarios javadoc

--- operacion_escape_algoritmo/src/operacion_escape_algoritmo/Habitaciones.java
+++ operacion_escape_algoritmo/src/operacion_escape_algoritmo/Habitaciones.java
@@ -1,18 +1,34 @@
 package operacion_escape_algoritmo;
 
-//Clase que representa las habitaciones
+/**
+ * Clase que representa las habitaciones.
+ */
 public class Habitaciones {
-	private int habitacion;
-	
-	public Habitaciones() {
-		this.habitacion = 0;
+    private int habitacion;
+
+    /**
+     * Constructor por defecto de la clase Habitaciones.
+     * Inicializa la habitación a 0.
+     */
+    public Habitaciones() {
+        this.habitacion = 0;
     }
-	
-	public int getHabitacion() {
+
+    /**
+     * Obtiene el número de habitación.
+     *
+     * @return El número de habitación.
+     */
+    public int getHabitacion() {
         return habitacion;
     }
-	
-	public void setHabitacion(int habitacion) {
+
+    /**
+     * Establece el número de habitación.
+     *
+     * @param habitacion El número de habitacion a establecer.
+     */
+    public void setHabitacion(int habitacion) {
         this.habitacion = habitacion;
     }
 }
